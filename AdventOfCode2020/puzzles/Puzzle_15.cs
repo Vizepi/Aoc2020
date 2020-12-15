c009569c1b1f9304688558dedf77f2a2576bae2c
Joseph Kieffer <joseph.kieffer@hotmail.fr>
2020-12-15 10:06:57 +0100

    Added puzzle 15

--- AdventOfCode2020/puzzles/Puzzle_15.cs
+++ AdventOfCode2020/puzzles/Puzzle_15.cs
@@ -1,17 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AdventOfCode2020.puzzles {
-	class Puzzle_15 : IPuzzle {
+	abstract class Puzzle_15 : IPuzzle {
 		public long Resolve( string input ) {
-			return 0;
+			string[] numberStrs = input.Split( new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries );
+
+			long[] previous = new long[ Iterations ];
+			for ( long i = 0; i < Iterations; ++i ) {
+				previous[ i ] = -1;
+			}
+
+			long last = 0;
+
+			for ( long i = 0; i < numberStrs.Length; ++i ) {
+				if ( !long.TryParse( numberStrs[ i ], out last ) ) {
+					return -1;
+				}
+
+				previous[ last ] = i;
+			}
+
+			for ( int i = numberStrs.Length - 1; i < Iterations - 1; ++i ) {
+				if ( previous[ last ] >= 0 ) {
+					long diff = i - previous[ last ];
+					previous[ last ] = i;
+					last = diff;
+				} else {
+					previous[ last ] = i;
+					last = 0;
+				}
+			}
+
+			return last;
 		}
 
 		public string ErrorCodeToString( long code ) {
-			return "";
+			switch ( code ) {
+			default:
+				if ( code < 0 ) {
+					return "Unknown error";
+				}
+				return "No error";
+			}
+		}
+
+		protected abstract long Iterations {
+			get;
 		}
 	}
 
-	class Puzzle_15_1 : Puzzle_15 { }
+	class Puzzle_15_1 : Puzzle_15 {
+		protected override long Iterations {
+			get {
+				return 2020;
+			}
+		}
+	}
 
-	class Puzzle_15_2 : Puzzle_15 { }
+	class Puzzle_15_2 : Puzzle_15 {
+		protected override long Iterations {
+			get {
+				return 30000000;
+			}
+		}
+	}
 }
